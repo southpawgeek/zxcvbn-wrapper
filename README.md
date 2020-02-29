98be3930199b896ee3515ef80f7013eaa694e96c
southpawgeek <jen@southpawgeek.com>
2020-02-29 10:33:28 -0500

    neatened markup, removed Material icons

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # zxcvbn-wrapper
-This is a super basic implementation of [zxcvbn](https://github.com/dropbox/zxcvbn).
+## super basic, dependency-less implementation of [zxcvbn](https://github.com/dropbox/zxcvbn)
+
+Plain old vanilla JS. **No JS libraries required**, aside from [zxcvbn](https://github.com/dropbox/zxcvbn) itself. This example is using [cdnjs.com](https://cdnjs.com/libraries/zxcvbn) to load [zxcvbn](https://github.com/dropbox/zxcvbn).
+
+The feedback warnings and suggestions are unmodified. They are hard-coded into [zxcvbn](https://github.com/dropbox/zxcvbn).
--- wrapper.js
+++ wrapper.js
@@ -1,44 +1,49 @@
-var pwshow = document.getElementById("pwshow");
-var pwfield = document.getElementById("pword");
-var pwwarn = document.getElementById("warning");
-var pwsugg = document.getElementById("suggestions");
-var pwbar = document.getElementById("progress_bar");
-var pwstar = document.getElementById("pwstar");
-
-pwshow.onclick = function () {
+document.addEventListener("DOMContentLoaded", function () {
     'use strict';
-    if (pwshow.checked === true) {
-        pwfield.type = 'text';
-    } else {
-        pwfield.type = 'password';
-    }
-};
 
-pwfield.onkeyup = function () {
-    'use strict';
-    var result = zxcvbn(pwfield.value);
-    var pwlength = pwfield.value.length;
-    var barsize = result.score * 25;
-    var star_pos = '<i class="material-icons">star</i>';
-    var star_neg = '<i class="material-icons">star_outline</i>';
-    star_pos = star_pos.repeat(result.score);
-    star_neg = star_neg.repeat(4 - result.score);
+    // these are your filled/unfilled stars
+    var star_filled = '<span class="zw-progress__star--fill">&starf;</span>';
+    var star_empty = '<span class="zw-progress__star--empty">&star;</span>';
 
-    pwstar.innerHTML = star_pos + star_neg;
+    var pwshow = document.getElementById("zw-password__show--check");
+    var pwfield = document.getElementById("zw-password__input");
+    var pwwarn = document.getElementById("zw-feedback__warning");
+    var pwsugg = document.getElementById("zw-feedback__suggestion");
+    var pwbar = document.getElementById("zw-progress__bar--fill");
+    var pwstar = document.getElementById("zw-progress__star");
 
-    pwbar.className = '';
-    pwbar.classList.add("score" + barsize);
-    pwwarn.innerHTML = result.feedback.warning;
-    pwsugg.innerHTML = result.feedback.suggestions;
-
-    if (result.score === 3) {
-        if (pwlength < 11) {
-            pwwarn.innerHTML = 'Try adding more characters.';
+    pwshow.onclick = function () {
+        if (pwshow.checked === true) {
+            pwfield.type = 'text';
         } else {
-            pwwarn.innerHTML = 'Just a little bit more...';
+            pwfield.type = 'password';
         }
-    }
-    if (result.score === 4) {
-        pwwarn.innerHTML = 'Strong password!';
-    }
-};
+    };
+
+    pwfield.onkeyup = function () {
+        var result = zxcvbn(pwfield.value);
+        var pwlength = pwfield.value.length;
+        var barsize = result.score * 25;
+        var star_pos = star_filled.repeat(result.score);
+        var star_neg = star_empty.repeat(4 - result.score);
+
+        pwstar.innerHTML = star_pos + star_neg;
+
+        pwbar.className = '';
+        pwbar.classList.add("zw-progress__bar--score" + barsize);
+        pwwarn.innerHTML = result.feedback.warning;
+        pwsugg.innerHTML = result.feedback.suggestions;
+
+        if (result.score === 3) {
+            if (pwlength < 11) {
+                pwwarn.innerHTML = 'Try adding more characters.';
+            } else {
+                pwwarn.innerHTML = 'Just a little bit more...';
+            }
+        }
+        if (result.score === 4) {
+            pwwarn.innerHTML = 'Strong password!';
+        }
+    };
+
+});
